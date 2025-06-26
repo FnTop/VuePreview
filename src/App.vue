9143360b736abffbdc845ec9986af2588a1efa8a
lelunqiang <442232278@qq.com>
2025-06-26 23:49:34 +0800

    更新文件以修复样式问题并优化性能，增强用户体验

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app">
+		<el-container>
+			<el-header>工具站点</el-header>
+			<el-main>
+				<code-preview />
+			</el-main>
+		</el-container>
+	</div>
+</template>
+
+<script setup>
+	import CodePreview from "./components/CodePreview.vue";
+</script>
+
+<style lang="less" scoped>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
